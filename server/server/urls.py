ce1a822d58a2c2f95f7454703ed7ea38a25155e5
haixei <aleksandraosuch002@gmail.com>
2021-06-03 15:00:01 +0200

    Update routes

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from ..way import views
+from way.views import *
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserView, 'user')
-router.register(r'comments', views.CommentView, 'comment')
-router.register(r'postedimages', views.UserView, 'postedimage')
+router.register(r'users', UserView, 'user')
+router.register(r'comments', CommentView, 'comment')
+router.register(r'postedimages', UserView, 'postedimage')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
