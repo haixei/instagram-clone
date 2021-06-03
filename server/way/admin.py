286d2f423c898d5cc0b9e3af696dc6f989036177
haixei <aleksandraosuch002@gmail.com>
2021-06-03 14:28:23 +0200

    Register models, linting

--- server/way/admin.py
+++ server/way/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import User, Comment, PostedImage
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'bio', 'created', 'following')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('content', 'author')
+
+
+class PostedImageAdmin(admin.ModelAdmin):
+    list_display = ('imgURL', 'description', 'created')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(PostedImage, PostedImageAdmin)
--- server/way/models.py
+++ server/way/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
+
 # Create the models
 class User(models.Model):
     username = models.CharField(max_length=50)
@@ -12,6 +13,7 @@ class User(models.Model):
         )
     )
 
+
 class Comment(models.Model):
     content = models.CharField(max_length=200)
     author = models.ForeignKey(
@@ -19,7 +21,8 @@ class Comment(models.Model):
         on_delete=models.CASCADE,
     )
 
-class Image(models.Model):
+
+class PostedImage(models.Model):
     imgURL = models.CharField(max_length=120)
     description = models.CharField(max_length=200)
     created = models.DateField()
