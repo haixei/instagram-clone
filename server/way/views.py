7eee43d406b35dc35f4a8f7efab53653caa6ee79
haixei <aleksandraosuch002@gmail.com>
2021-06-03 14:47:31 +0200

    Create views and register them in the router

--- server/way/views.py
+++ server/way/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import UserSerializer, CommentSerializer, PostedImageSerializer
+from .models import User, Comment, PostedImage
 
-# Create your views here.
+
+class UserView(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class CommentView(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
+
+
+class PostedImageView(viewsets.ModelViewSet):
+    serializer_class = PostedImageSerializer
+    queryset = PostedImage.objects.all()
