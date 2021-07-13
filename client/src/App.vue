9596343c5fbfdca63f49753021b5b0e6dc9d9671
haixei <aleksandraosuch002@gmailcom>
2021-07-13 13:07:49 +0200

    Disable navigation on the login page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- v-if statement hides the navigation on the log in page -->
+    <div id="nav" v-if="['LogIn'].indexOf($route.name) == -1">
         <router-link to="/">Home</router-link> |
         <router-link to="/about">About</router-link>
     </div>
@@ -8,26 +9,3 @@
     </router-view>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #a135df;
-    }
-  }
-}
-</style>
