9465057dd545ca70e6d750303b8e6292b8f684c1
haixei <aleksandraosuch002@gmailcom>
2021-11-23 20:17:30 +0100

    Start writing tests based on the documentation

--- server/instagram/tests/test_views.py
+++ server/instagram/tests/test_views.py
@@ -1,19 +1,47 @@
 from django.test import TestCase, Client
+from ..models import *
+import tempfile
+from django.contrib.auth import get_user_model
+
+email = 'testemail@example.com'
+password = 'testpass123'
 
 
+# Doc. MVP 1.1
 class ProfileTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(username='testuser', email=email,
+                                                        password=password)
+
     def test_profile_access(self):
-        # Everyone without the admin privileges should be able to view the profiles
-        response = self.client.get('/api/profiles/')
+        # Anyone should be able to get a profile by the username
+        response = self.client.get('/api/profiles/username/' + self.user.username, follow=True)
         self.assertEqual(response.status_code, 200)
 
-    def test_retrieve_wrong_uid(self):
-        # Any user id that is not a string or an invalid id should return a 404 code
-        response = self.client.get('/api/profiles/abc/')
-        self.assertEqual(response.status_code, 404)
+    def test_profile_access_own(self):
+        # Owner of its own data should be able to view it in private mode
+        self.assertTrue(self.client.login(email=email, password=password))
+        response = self.client.get('/api/profiles/username' + self.user.username, follow=True)
+        self.assertEqual(response.status_code, 200)
 
-    def test_retrieve_session_user(self):
-        # We're testing if the permissions work correctly, we only
-        # want to respond with data if the user has an active session id
-        response = self.client.get('/api/profiles/sessionUser', follow=True)
-        self.assertEqual(response.status_code, 404)
+    def test_profile_retrieve_access(self):
+        response = self.client.get('/api/profiles/', follow=True)
+        self.assertEqual(response.status_code, 401)
+
+
+# Doc. MVP 2.1
+class ImagesTestCase(TestCase):
+    def test_image_post(self):
+        user = get_user_model().objects.create_user(username='testuser', email='testemail@example.com')
+
+        PostedImage.objects.create(
+            description='Test description.',
+            created=datetime.now(),
+            hashtags=['beach'],
+            author=user,
+            image=tempfile.NamedTemporaryFile(suffix=".jpg").name,
+        )
+
+        response = self.client.get('/api/postedimages/1', follow=True)
+        self.assertEqual(response.status_code, 200)
