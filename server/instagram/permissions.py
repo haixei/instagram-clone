305911fe15c993efb5ad588550f4ce2e3882ef38
haixei <aleksandraosuch002@gmailcom>
2021-12-01 19:02:39 +0100

    Remove unused permissions

--- server/instagram/permissions.py
+++ server/instagram/permissions.py
@@ -2,15 +2,11 @@ from rest_framework import permissions
 from rest_framework.permissions import IsAdminUser, SAFE_METHODS
 
 
-class IsAdminOrReadOnly(IsAdminUser):
-    def has_permission(self, request, view):
-        is_admin = super().has_permission(request, view)
-        return request.method in SAFE_METHODS or is_admin
-
-
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
         try:
-            return (obj.author == request.user) or request.method in SAFE_METHODS
+            return obj.author == request.user
         except AttributeError:
             return obj.user == request.user
